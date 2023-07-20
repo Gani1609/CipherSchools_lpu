4e376054ea0448a85e325a28fb09023e8212b0f7
Gani1609 <138689070+Gani1609@users.noreply.github.com>
2023-07-20 10:49:14 +0530

    Update lecture-24.cpp

--- lecture-24.cpp
+++ lecture-24.cpp
@@ -3,25 +3,22 @@ using namespace std;
 
 int fib(int input)
 {
-    if(input==0 || input=-1)
+    if (input == 0 || input == 1)
     {
-        return 1;
+        return input;
     }
-    int Final_answer;
-
-    Final_answer = fib(input-1) + fib(input-2);
+    
+    int Final_answer = fib(input - 1) + fib(input - 2);
     return Final_answer;
 }
-
 
 int main()
 {
     int input;
-    cout<<"enter the number you want to find the fibonicci series to: "<<endl;
-    cin>>input;
+    cout << "Enter the number you want to find the Fibonacci series term for: " << endl;
+    cin >> input;
 
-    cout<<fib(input);
+    cout << "Fibonacci term at position " << input << ": " << fib(input) << endl;
 
     return 0;
-
 }
