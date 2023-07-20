971c2a8ba0f67516e48f97b9a943cde0673e60d2
Gani1609 <138689070+Gani1609@users.noreply.github.com>
2023-07-20 12:44:57 +0530

    Update lecture-29-I.cpp

--- lecture-29-I.cpp
+++ lecture-29-I.cpp
@@ -1,143 +1,103 @@
-#include<iosteam>
+#include <iostream>
 using namespace std;
 
 class node
 {
-    public:
-
+public:
     int data;
-    node * next;
+    node* next;
 
     node(int data_input)
     {
-        this->data =data_input;
+        this->data = data_input;
         this->next = NULL;
     }
 };
 
-void printnode(node * &node)
+void printnode(node* nd)
 {
-    cout<<"value " <<node->data;
-    cout<<"address"<<node->next;
+    cout << "value: " << nd->data;
+    cout << ", address: " << nd << endl;
 }
 
-void printlinkedist(node * &head)
+void printlinkedist(node* head)
 {
-    if(head==NULL)
+    if (head == NULL)
     {
-        cout<<"the linked list is still empty!!"<<endl;
-        return ;
+        cout << "The linked list is still empty!!" << endl;
+        return;
     }
 
-    node *temp;
-    temp=head;
+    node* temp = head;
 
-    while(temp!=NULL)
+    while (temp != NULL)
     {
-        cout<<temp->data<<"-->";
-        temp=temp->next;
+        cout << temp->data;
+        if (temp->next != NULL)
+            cout << "-->";
+        temp = temp->next;
     }
-    cout<<endl;
+
+    cout << endl;
 }
 
-void Insertstart(node *head,int data)
+void Insertstart(node* &head, int data)
 {
-    node * temp=new node(data);
+    node* temp = new node(data);
 
-    if(head==NULL)
+    if (head == NULL)
     {
         head = temp;
         return;
     }
 
-    temp->next=head;
-    head=temp;
+    temp->next = head;
+    head = temp;
 }
 
-void Insertend(node *&head,int data)
+void Insertend(node* &head, int data)
 {
-    node *temp=new node(data);
+    node* temp = new node(data);
 
-
-    if(head==NULL)
+    if (head == NULL)
     {
         head = temp;
         return;
     }
 
-    node *end;
-    end = head;
+    node* end = head;
 
-    while(end->next!=NULL)
+    while (end->next != NULL)
     {
         end = end->next;
     }
 
-     end->next = temp;
+    end->next = temp;
 }
-
 
 int main()
 {
+    node* head = NULL;
 
-    // node*head;
+    Insertend(head, 5);
+    Insertend(head, 7);
+    Insertend(head, 8);
 
-    // // printlinkedist(head);
+    Insertstart(head, 3);
+    Insertstart(head, 2);
+    Insertstart(head, 9);
+    Insertstart(head, 10);
 
-    // Insertend(5);
-    // Insertend(7);
-    // Insertend(8);
+    printlinkedist(head);
 
-    // node * node1 = new node(5);
-    // head = node1;
-    // // printlinkedist(head);
-    // // cout<<endl;
-
-    // node * node2 = new node(7);
-    // node1->next = node2;
-    // // printlinkedist(head);
-    // // cout<<endl;
-
-    // node * node3 = new node(8);
-    // node2->next = node3;
-    // printlinkedist(head);
-    // cout<<endl;
-
-    // cout<<node1->data;
-    // printnode(node1);
-    // printnode(node2);
-    // printnode(node3);
-
-    
-    
-
-    // printnode(node1);
-    // cout<<endl<<node2;
-    // printnode(node2);
-    // cout<<endl<<node3;
-    // printnode(node3);
-    // printnode(node1->next);
-    // printnode(node1->next->next);
-
-    // node * head;
-    // head = node1;
-
-   node*head;
-
-    // printlinkedist(head);
-
-    Insertend(head,5);
-    Insertend(head,7);
-    Insertend(head,8);
-    
-Insertstart(head,3);
-// printlinkedist(head);
-Insertstart(head,2);
-// printlinkedist(head);
-Insertstart(head,9);
-Insertstart(head,10);
-
-printlinkedist(head);
+    // Remember to free the allocated memory
+    node* current = head;
+    while (current != NULL)
+    {
+        node* next_node = current->next;
+        delete current;
+        current = next_node;
+    }
 
     return 0;
 }
