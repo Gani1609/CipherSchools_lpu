504e3a656764c032db959610f24dba24e643bf0f
Gani1609 <138689070+Gani1609@users.noreply.github.com>
2023-07-26 18:08:03 +0530

    Create "THIS"pointer.cpp

---
+++ "THIS"pointer.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+class Person {
+private:
+    std::string name;
+    int age;
+
+public:
+    Person(std::string name, int age) : name(name), age(age) {}
+
+    void printInfo() {
+        std::cout << "Name: " << this->name << ", Age: " << this->age << std::endl;
+    }
+};
+
+int main() {
+    Person person1("John", 30);
+    Person person2("Alice", 25);
+
+    // Call the printInfo() function using "this" pointer
+    std::cout << "Person 1's info: ";
+    person1.printInfo();
+
+    std::cout << "Person 2's info: ";
+    person2.printInfo();
+
+    return 0;
+}
