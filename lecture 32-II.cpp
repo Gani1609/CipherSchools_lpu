797d3e36ef8a8eae3b5addd3800363cc01dcc872
Gani1609 <138689070+Gani1609@users.noreply.github.com>
2023-07-28 12:18:49 +0530

    Update lecture 32-II.cpp

--- lecture 32-II.cpp
+++ lecture 32-II.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int partition()
+int partition(int *arr, int start, int end)
 {
-    int i , number_lessthan_part;
-    for(i=start+1,number_lessthan_part=0;i<end+1;i++)
+    int i, number_lessthan_part;
+    for (i = start + 1, number_lessthan_part = 0; i < end + 1; i++)
     {
-        if(arr[i]<arr[start])
+        if (arr[i] < arr[start])
         {
             number_lessthan_part++;
         }
@@ -16,30 +16,28 @@ int partition()
 
     correct_index = start + number_lessthan_part;
 
-
     int temp;
 
-    temp=arr[start];
+    temp = arr[start];
     arr[start] = arr[correct_index];
     arr[correct_index] = temp;
 
-    int i,j;
-    i=start;
-    j=end;
+    i = start;
+    int j = end;
 
-    while(i<correct_index && j>correct_index)
+    while (i < correct_index && j > correct_index)
     {
-        while(arr[i]<arr[correct_index])
+        while (arr[i] < arr[correct_index])
         {
             i++;
         }
 
-        while(arr[j]>correct_index)
+        while (arr[j] > arr[correct_index])
         {
             j--;
         }
 
-        if(i<correct_index && j>correct_index)
+        if (i < correct_index && j > correct_index)
         {
             temp = arr[i];
             arr[i] = arr[j];
@@ -49,38 +47,38 @@ int partition()
             j--;
         }
     }
+
+    return correct_index;
 }
 
-void quicksort(int *arr, int start ,int end)
+void quicksort(int *arr, int start, int end)
 {
-
-    if(start>=end)
+    if (start >= end)
     {
         return;
     }
 
     int partition_index;
-    partition_index = partition(arr , start ,end)
+    partition_index = partition(arr, start, end);
 
-   quicksort(arr,start,partition-1);
-   quicksort(arr,partition+1,end);
+    quicksort(arr, start, partition_index - 1);
+    quicksort(arr, partition_index + 1, end);
 }
 
 int main()
 {
     int size;
 
-    int arr[7]={5,1,3,10,7,14,2};
-    
-    size=7;
+    int arr[7] = {5, 1, 3, 10, 7, 14, 2};
+    size = 7;
 
-    quicksort(arr,0,size-1);
+    quicksort(arr, 0, size - 1);
 
     int k;
 
-    for(k=0;k<size;k++)
+    for (k = 0; k < size; k++)
     {
-        cout<<arr[k]<<" ";
+        cout << arr[k] << " ";
     }
 
     return 0;
